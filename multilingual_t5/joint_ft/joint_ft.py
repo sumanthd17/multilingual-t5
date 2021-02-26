a7607d5bef8e943348e9f6047630f933d4d8d69f
Sumanth Doddapaneni <dsumanth17@gmail.com>
2021-02-26 15:37:18 +0530

    update path

--- multilingual_t5/joint_ft/joint_ft.py
+++ multilingual_t5/joint_ft/joint_ft.py
@@ -47,8 +47,8 @@ class JointFt(tfds.core.GeneratorBasedBuilder):
     # TODO(hi_en): Returns the Dict[split names, Iterator[Key, Example]]
     return {
         'train': self._generate_examples(source=train/'j-train/joint-train.txt', target=train/'j-train/en-train.txt'),
-        'validation': self._generate_examples(source=train/'j-dev/joint-dev.txt', target=train/'j-dev/en-dev.txt'),
-        'test': self._generate_examples(source=train/'j-test/joint-test.txt', target=train/'j-test/en-test.txt')
+        'validation': self._generate_examples(source=val/'j-dev/joint-dev.txt', target=val/'j-dev/en-dev.txt'),
+        'test': self._generate_examples(source=test/'j-test/joint-test.txt', target=test/'j-test/en-test.txt')
     }
 
   def _generate_examples(self, source, target):
