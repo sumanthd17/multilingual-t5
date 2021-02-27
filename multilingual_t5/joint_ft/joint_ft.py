1af32b97a0a69a4aa60896696947df705d76cc82
Sumanth Doddapaneni <dsumanth17@gmail.com>
2021-02-27 10:03:49 +0530

    change read

--- multilingual_t5/joint_ft/joint_ft.py
+++ multilingual_t5/joint_ft/joint_ft.py
@@ -2,6 +2,7 @@
 
 import tensorflow_datasets as tfds
 import tensorflow as tf
+import random
 
 # TODO(joint_ft): Markdown description  that will appear on the catalog page.
 _DESCRIPTION = """
@@ -41,22 +42,47 @@ class JointFt(tfds.core.GeneratorBasedBuilder):
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
     # TODO(joint_ft): Downloads the data and defines the splits
-    train = dl_manager.download_and_extract('https://storage.googleapis.com/ai4b-anuvaad-nmt/joint-ft/data/j-train.zip')
-    val = dl_manager.download_and_extract('https://storage.googleapis.com/ai4b-anuvaad-nmt/joint-ft/data/j-dev.zip')
-    test = dl_manager.download_and_extract('https://storage.googleapis.com/ai4b-anuvaad-nmt/joint-ft/data/j-test.zip')
+    bn = dl_manager.download_and_extract('https://anuvaad-parallel-corpus.s3-us-west-2.amazonaws.com/train-2021-v1-en-hi.zip')
+    hi = dl_manager.download_and_extract('https://anuvaad-parallel-corpus.s3-us-west-2.amazonaws.com/train-2021-v1-en-bn.zip')
+    ta = dl_manager.download_and_extract('https://anuvaad-parallel-corpus.s3-us-west-2.amazonaws.com/train-2021-v1-en-ta.zip')
+
+    devtest = dl_manager.download_and_extract('https://anuvaad-parallel-corpus.s3-us-west-2.amazonaws.com/devtest-2021-v1.zip')
 
     # TODO(joint_ft): Returns the Dict[split names, Iterator[Key, Example]]
     return {
-        'train': self._generate_examples(source=train/'j-train/joint-train.txt', target=train/'j-train/en-train.txt'),
-        'validation': self._generate_examples(source=val/'j-dev/joint-dev.txt', target=val/'j-dev/en-dev.txt'),
-        'test': self._generate_examples(source=test/'j-test/joint-test.txt', target=test/'j-test/en-test.txt')
+        'train': self._generate_examples(bn_src=bn/'train.bn', bn_tgt=bn/'train.en', hi_src=hi/'train.hi', hi_tgt=hi/'train.en', ta_src=ta/'train.ta', ta_tgt=ta/'train.en'),
+        'validation': self._generate_examples(bn_src=devtest/'all/en-bn/dev.bn', bn_tgt=devtest/'all/en-bn/dev.en', hi_src=devtest/'all/en-hi/dev.hi', hi_tgt=devtest/'all/en-hi/dev.en', ta_src=devtest/'all/en-ta/dev.ta', ta_tgt=devtest/'all/en-ta/dev.en'),
+        'test': self._generate_examples(bn_src=devtest/'all/en-bn/test.bn', bn_tgt=devtest/'all/en-bn/test.en', hi_src=devtest/'all/en-hi/test.hi', hi_tgt=devtest/'all/en-hi/test.en', ta_src=devtest/'all/en-ta/test.ta', ta_tgt=devtest/'all/en-ta/test.en'),
     }
 
-  def _generate_examples(self, source, target):
+  def _generate_examples(self, bn_src, bn_tgt, hi_src, hi_tgt, ta_src, ta_tgt):
     """Yields examples."""
     # TODO(joint_ft): Yields (key, example) tuples from the dataset
-    src = tf.io.gfile.GFile(source, mode='r').readlines()
-    tgt = tf.io.gfile.GFile(target, mode='r').readlines()
+    bn_src = tf.io.gfile.GFile(bn_src, mode='r').readlines()
+    bn_tgt = tf.io.gfile.GFile(bn_tgt, mode='r').readlines()
+
+    hi_src = tf.io.gfile.GFile(hi_src, mode='r').readlines()
+    hi_tgt = tf.io.gfile.GFile(hi_tgt, mode='r').readlines()
+
+    ta_src = tf.io.gfile.GFile(ta_src, mode='r').readlines()
+    ta_tgt = tf.io.gfile.GFile(ta_tgt, mode='r').readlines()
+
+    src = []
+    tgt = []
+
+    src.extend(bn_src)
+    src.extend(hi_src)
+    src.extend(ta_src)
+
+    tgt.extend(bn_tgt)
+    tgt.extend(hi_tgt)
+    tgt.extend(ta_tgt)
+
+    temp = list(zip(a,b))
+    random.shuffle(temp)
+
+    src, tgt = zip(*temp)
+
     for idx, row in enumerate(zip(src, tgt)):
       yield idx, {
         'source': row[0],
